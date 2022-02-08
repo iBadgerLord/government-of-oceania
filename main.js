ed32378def887cc8a490540777e96705d1746eba
iBadgerLord <52838484+iBadgerLord@users.noreply.github.com>
2022-02-08 18:09:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,10 @@ function colorLink(){
 }
 linkColor.forEach(l=> l.addEventListener('click', colorLink))
 
+
+
 /*===== WORD OF THE DAY PROGRAMME =====*/
+
 // Exact date programme
 const monthOfYear = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
 const d = new Date();
