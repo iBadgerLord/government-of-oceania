c15851f7f7d3b895f3c43536d145567de330e398
iBadgerLord <52838484+iBadgerLord@users.noreply.github.com>
2022-02-10 17:49:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -36,69 +36,85 @@ linkColor.forEach(l=> l.addEventListener('click', colorLink))
 
 
 /*===== WORD OF THE DAY PROGRAMME =====*/
-
+ 
 // Exact date programme
-const monthOfYear = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
+const monthOfYear = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const d = new Date();
 let todayMonth = monthOfYear[d.getMonth()]
 let todayYear = d.getFullYear();
 let todayDay = d.getDate();
 document.getElementById("word-of-the-day-DATE").innerHTML = todayMonth + " " + todayDay + ", " + todayYear;
+ 
+// Programme for word of the day
+const wordOTDList = ["doublethink", "crimethink", "crimestop", "bellyfeel", "facecrime", "goodthink", "duckspeak"];
+const wordOTDDefinitionList = [
+    "'Reality control. The power to hold two completely contradictory beliefs in one's mind simultaneously, and accept both of them.'", 
+    "'Doubting any of the principles of Ingsoc.'",
+    "'To rid oneself of unorthodox thoughts that go against Ingsoc's ideology.'",
+    "'The blind, enthusiastic acceptance of a concept.'",
+    "'A facial expression which reveals that one has committed thoughtcrime.'",
+    "'Political orthodoxy.'",
+    "'Automatic, vocal support of political orthodoxies.'"
+];
+let todayWeekDay = d.getDay();
+var wordOfTheDayNumber = 0;
+var wordOfTheDay = wordOTDList[wordOfTheDayNumber];
 
+document.getElementById("wordDefinition").innerHTML = wordOTDDefinitionList[wordOfTheDayNumber];
+ 
 // Programme variables
 var button = document.getElementById("submitGuessButton");
-
-var wordOfTheDay = "doublethink";
+ 
 const secretKey = ["down", "with", "big", "brother"];
 const userKey = [];
-
+ 
 var guessed = false;
 var userScore = 0;
 var guesses = 0;
-
-
+ 
+ 
 // On button click programme
 function wordOfTheDayButtonClick() {
-
-    var userWord = document.getElementById("wordOfTheDayGuessBox").value;
-    userKey.push(userWord);
-    guesses += 1;
-
-    if (userWord === wordOfTheDay) {
-        // says the word is correct
-        //   document.getElementById("result").innerHTML = "Congratulations comrade!";
-        // removes ability of more guesses
-        button.disabled = true;
-        // disables input field
-        document.getElementById("wordOfTheDayGuessBox").disabled = true;
-        
-    } else {
-
-        if (guesses === 4) {
-
-            for (let i = 0; i <= 3; i++) {
-                if (userKey[i] === secretKey[i]) {
-                    userScore += 1;
-                }
-            }
-
-            if (userScore === 4) {
-                // says the word is correct
-                //   document.getElementById("result").innerHTML = "you are in...";
-                window.open('brotherhood.html');
-                document.getElementById("wordOfTheDayGuessBox").value = "you are dead";
-            } 
-            // removes ability of more guesses
-            button.disabled = true;
-            // disables input field
-            document.getElementById("wordOfTheDayGuessBox").disabled = true;
-        } 
-    }
-
-    if (userWord === wordOfTheDay) {
-        document.getElementById("wordOfTheDayGuessBox").value = "doublethink";
-    } else if ((guesses != 4) || ((guesses === 4) && (userScore != 4)) ) {
-        document.getElementById("wordOfTheDayGuessBox").value = "";
-    }
-
+ 
+   var userWord = document.getElementById("wordOfTheDayGuessBox").value;
+   userKey.push(userWord);
+   guesses += 1;
+ 
+   if (userWord === wordOfTheDay) {
+       // says the word is correct
+       //   document.getElementById("result").innerHTML = "Congratulations comrade!";
+       // removes ability of more guesses
+       button.disabled = true;
+       // disables input field
+       document.getElementById("wordOfTheDayGuessBox").disabled = true;
+      
+   } else {
+ 
+       if (guesses === 4) {
+ 
+           for (let i = 0; i <= 3; i++) {
+               if (userKey[i] === secretKey[i]) {
+                   userScore += 1;
+               }
+           }
+ 
+           if (userScore === 4) {
+               // says the word is correct
+               //   document.getElementById("result").innerHTML = "you are in...";
+               window.open('brotherhood.html');
+               document.getElementById("wordOfTheDayGuessBox").value = "you are dead";
+           }
+           // removes ability of more guesses
+           button.disabled = true;
+           // disables input field
+           document.getElementById("wordOfTheDayGuessBox").disabled = true;
+       }
+   }
+ 
+   if (userWord === wordOfTheDay) {
+       document.getElementById("wordOfTheDayGuessBox").value = wordOfTheDay;
+   } else if ((guesses != 4) || ((guesses === 4) && (userScore != 4)) ) {
+       document.getElementById("wordOfTheDayGuessBox").value = "";
+   }
+ 
 }
