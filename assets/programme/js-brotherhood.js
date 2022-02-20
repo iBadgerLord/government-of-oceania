032ef7b9bc78b535c7076b0dc2e839ea5359417b
iBadgerLord <52838484+iBadgerLord@users.noreply.github.com>
2022-02-20 16:33:56 +0100

    Update js-brotherhood.js

--- assets/programme/js-brotherhood.js
+++ assets/programme/js-brotherhood.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-    $('#title').focus();
-      $('#text').autosize();
-  });
-  
-
 // On button click archives appear
 function showDiv(targetDiv) {
     targetDiv.style.display = "block";
 }
 
+// On button click signs up to event
 function signUp(targetDiv) {
     document.getElementById(targetDiv).innerHTML = "yes";
     document.getElementById(targetDiv).style.color = "#00dd00";
+}
+
+// On button tries to download fake file -- fails
+function failedDownload() {
+    alert("Download failed");
 }
